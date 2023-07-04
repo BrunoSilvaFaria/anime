e0912b84fe03a1642382d8748bf188469a841fc0
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-04 15:57:35 -0300

    feat: arquivo responsável por receber os comentários

---
+++ src/data/dataReview.js
@@ -0,0 +1,66 @@
+const dataReview = [
+    {   
+        coment: 'whachikan Just noticed that someone categorized this as belonging to the genre "demons" LOL',
+        img: '/assets/anime/review-1.jpg',
+        time: '1 Hour ago',
+        user: 'Chris Curry'
+    },
+    {   
+        coment: 'Finally it came out ages ago',
+        img: '/assets/anime/review-2.jpg',
+        time: '5 Hour ago',
+        user: 'Lewis Mann'
+    },
+    {   
+        coment: 'Where is the episode 15 ? Slow update! Tch',
+        img: '/assets/anime/review-3.jpg',
+        time: '20 Hour ago',
+        user: 'Louis Tyler'
+    },
+    
+    {   
+        coment: 'whachikan Just noticed that someone categorized this as belonging to the genre "demons" LOL',
+        img: '/assets/anime/review-4.jpg',
+        time: '1 Hour ago',
+        user: 'Chris Curry'
+    },
+
+    {   
+        coment: 'Finally it came out ages ago',
+        img: '/assets/anime/review-5.jpg',
+        time: '5 Hour ago',
+        user: 'Lewis Mann'
+    },
+    {   
+        coment: 'Where is the episode 15 ? Slow update! Tch',
+        img: '/assets/anime/review-6.jpg',
+        time: '20 Hour ago',
+        user: 'Louis Tyler'
+    }
+]
+export default dataReview;
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
