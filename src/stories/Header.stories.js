84d4eef1f9dd623d3a64cc70195af3aa9bce0aa9
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-04 15:58:34 -0300

    docs: documentação gerada pelo storybook

--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -3,7 +3,7 @@ import { Header } from './Header';
 export default {
   title: 'Example/Header',
   component: Header,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  // This component will have an automatically geneepisoded Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
