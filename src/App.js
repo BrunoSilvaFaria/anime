83eb75bfb0e21c94b7a2bd9aa1ed7956b13e3e48
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-05 16:08:56 -0300

    temp: exibição temporária de telas para desenvolvimento

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Header from './components/Header/Header';
 // import Home from './pages/Home/Home';
 import AnimeDetails from './pages/Anime Details/AnimeDetails';
 import Footer from './components/Footer/Footer';
+import Sign from './pages/Sign/Sign';
+import Login from './pages/Login/Login';
 import './global.css';
 function App() {
   return (
     <>
       <Header />
-      <AnimeDetails />
+      {/* <AnimeDetails /> */}
       {/* <Home /> */}
+      <Sign />
+      <Login />
       <Footer />
     </>
   )
