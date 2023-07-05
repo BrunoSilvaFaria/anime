9c25ce10171183fcf4cb2e45d6969b2ceb78942d
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-05 14:04:37 -0300

    temp: objeto responsável por receber informações de anime

---
+++ src/data/dataAnime.js
@@ -0,0 +1,46 @@
+const dataAnime = [
+    {   
+        type: 'TV Series',
+        studios: 'Lerche',
+        dataAired: 'Oct 02, 2019 to ?',
+        status: 'Airing',
+        genre: 'Action, Adventure, Fantasy, Magic',
+        score: '7.31 / 1,515',
+        rating: '8.5 / 161 times',
+        duration: '24 min/ep',
+        quality: 'HD',
+        views: '131,541',
+        title: 'Fate Stay Night: Unlimited Blade',
+        votes: '1.029 Votes',
+        description: "Every human inhabiting the world of Alcia is branded by a “Count” or a number written on their body. For Hina’s mother, her total drops to 0 and she’s pulled into the Abyss, never to be seen again. But her mother’s last words send Hina on a quest to find a legendary hero from the Waste War - the fabled Ace!",
+        subtitle: 'フェイト／ステイナイト, Feito／sutei naito',
+        stars: '4.7'
+    }
+]
+export default dataAnime;
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
