87a22de0eab4763f857af216144e852f13cf1820
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-06 16:37:42 -0300

    temp: estrutura temporaria para desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from './components/Header/Header';
 // import Home from './pages/Home/Home';
+import AnimeWatch from './pages/AnimeWatch/AnimeWatch';
+import AnimeEpisodes from './pages/AnimeEpisodes/AnimeEpisodes';
 import AnimeDetails from './pages/Anime Details/AnimeDetails';
 import Footer from './components/Footer/Footer';
 import Sign from './pages/Sign/Sign';
 import Login from './pages/Login/Login';
+import BlogDetails from './pages/BlogDetails/BlogDetails';
 import './global.css';
 function App() {
   return (
@@ -11,8 +14,11 @@ function App() {
       <Header />
       {/* <AnimeDetails /> */}
       {/* <Home /> */}
-      <Sign />
-      <Login />
+      {/* <Sign />
+      <Login /> */}
+      {/* <BlogDetails /> */}
+      {/* <AnimeWatch/> */}
+      <AnimeEpisodes />
       <Footer />
     </>
   )
