0b948f28eec7ad168b145dff1b290502dd8785b6
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-10 14:34:12 -0300

    feat: adição de rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import Header from './components/Header/Header';
-// import Home from './pages/Home/Home';
-import AnimeWatch from './pages/AnimeWatch/AnimeWatch';
-import AnimeEpisodes from './pages/AnimeEpisodes/AnimeEpisodes';
-import AnimeDetails from './pages/Anime Details/AnimeDetails';
 import Footer from './components/Footer/Footer';
-import Sign from './pages/Sign/Sign';
-import Login from './pages/Login/Login';
-import BlogDetails from './pages/BlogDetails/BlogDetails';
 import './global.css';
+
+// 2- Reaproveitando componentes
+import { Outlet } from 'react-router-dom';
 function App() {
   return (
     <>
       <Header />
-      {/* <AnimeDetails /> */}
-      {/* <Home /> */}
-      {/* <Sign />
-      <Login /> */}
-      {/* <BlogDetails /> */}
-      {/* <AnimeWatch/> */}
-      <AnimeEpisodes />
+
+      <Outlet />
+      
       <Footer />
     </>
   )
--- src/index.js
+++ src/index.js
@@ -3,9 +3,61 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
+import Home from './pages/Home/Home';
+import AnimeDetails from './pages/Anime Details/AnimeDetails';
+import AnimeEpisodes from './pages/AnimeEpisodes/AnimeEpisodes';
+import AnimeWatch from './pages/AnimeWatch/AnimeWatch';
+import BlogDetails from './pages/BlogDetails/BlogDetails'
+import Blog from './pages/Blog/Blog'
+import Login from './pages/Login/Login';
+import Sign from './pages/Sign/Sign';
+// Configurando router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { 
+        path: "/",
+        element: <Home />,
+      },
+      { 
+        path: "animeDetails",
+        element: <AnimeDetails />,
+      },
+      { 
+        path: "/animeEpisodes",
+        element: <AnimeEpisodes/>,
+      },
+      { 
+        path: "/animeWatch",
+        element: <AnimeWatch />,
+      },
+      { 
+        path: "/blog",
+        element: <Blog />,
+      },
+      { 
+        path: "/blogDetails",
+        element: <BlogDetails />,
+      },
+      { 
+        path: "login",
+        element: <Login />,
+      },
+      { 
+        path: "/sign",
+        element: <Sign />,
+      }
+    ]
+  }
+]);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App /> 
+  <RouterProvider router={router} />
 );
 
 reportWebVitals();
