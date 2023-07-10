72ca72cb7aeb4ec96ab683a1a220e9f50ce2e0ae
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-10 14:30:24 -0300

    feat: ajustes na estrutura, para facilitar na exibição de dados

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,66 +1,69 @@
-const dataBlog = [
-    {   
-        data: '01 March 2020',
-        title: 'Yuri Kuma Arashi Viverra Tortor Pharetra',
-        img: '/assets/blog/blog-1.jpg'
-    },
-
-    {   
-        data: '01 March 2020',
-        title: 'Fate/Stay Night: Untimated Blade World',
-        img: '/assets/blog/blog-2.jpg'
-    },
-    {   
-        data: '01 March 2020',
-        title: 'Building a Better LiA Drilling Down',
-        img: '/assets/blog/blog-3.jpg'
-    },
-    {   
-        data: '01 March 2020',
-        title: 'Bok no Hero Academia Season 4 – 18',
-        img: '/assets/blog/blog-4.jpg'
-    },
+const dataBlog = {
+    collumnMain: [
         {   
-        data: '01 March 2020',
-        title: 'Fate/Stay Night: Untimated Blade World',
-        img: '/assets/blog/blog-5.jpg'
-    },
-    {   
-        data: '01 March 2020',
-        title: 'Yuri Kuma Arashi Viverra Tortor Pharetra',
-        img: '/assets/blog/blog-6.jpg'
-    },
-    {   
-        data: '01 March 2020',
-        title: 'Housekishou Richard shi no Nazo Kantei Season 08 - 20',
-        img: '/assets/blog/blog-7.jpg'
-    },
-    {   
-        data: '01 March 2020',
-        title: 'Bok no Hero Academia Season 4 – 18',
-        img: '/assets/blog/blog-8.jpg'
-    },
-    {   
-        data: '01 March 2020',
-        title: 'Fate/Stay Night: Untimated Blade World',
-        img: '/assets/blog/blog-9.jpg'
-    },
-    {   
-        data: '01 March 2020',
-        title: 'Fate/Stay Night: Untimated Blade World',
-        img: '/assets/blog/blog-10.jpg'
-    },
-    {   
-        data: '01 March 2020',
-        title: 'Building a Better LiA Drilling Down',
-        img: '/assets/blog/blog-11.jpg'
-    }, 
-    {   
-        data: '01 March 2020',
-        title: 'Yuri Kuma Arashi Viverra Tortor Pharetra',
-        img: '/assets/blog/blog-12.jpg'
-    }
-]
+            data: '01 March 2020',
+            title: 'Yuri Kuma Arashi Viverra Tortor Pharetra',
+            img: '/assets/blog/blog-1.jpg'
+        },
+        
+        {   
+            data: '01 March 2020',
+            title: 'Bok no Hero Academia Season 4 – 18',
+            img: '/assets/blog/blog-4.jpg'
+        },
+            
+        {   
+            data: '01 March 2020',
+            title: 'Fate/Stay Night: Untimated Blade World',
+            img: '/assets/blog/blog-5.jpg'
+        },
+        {   
+            data: '01 March 2020',
+            title: 'Housekishou Richard shi no Nazo Kantei Season 08 - 20',
+            img: '/assets/blog/blog-7.jpg'
+        },
+        {   
+            data: '01 March 2020',
+            title: 'Fate/Stay Night: Untimated Blade World',
+            img: '/assets/blog/blog-9.jpg'
+        },
+        {   
+            data: '01 March 2020',
+            title: 'Building a Better LiA Drilling Down',
+            img: '/assets/blog/blog-11.jpg'
+        }],
+    collumnSec:[
+        {   
+            data: '01 March 2020',
+            title: 'Fate/Stay Night: Untimated Blade World',
+            img: '/assets/blog/blog-2.jpg'
+        },
+        {   
+            data: '01 March 2020',
+            title: 'Building a Better LiA Drilling Down',
+            img: '/assets/blog/blog-3.jpg'
+        },
+        {   
+            data: '01 March 2020',
+            title: 'Yuri Kuma Arashi Viverra Tortor Pharetra',
+            img: '/assets/blog/blog-6.jpg'
+        },
+        {   
+            data: '01 March 2020',
+            title: 'Bok no Hero Academia Season 4 – 18',
+            img: '/assets/blog/blog-8.jpg'
+        },
+        {   
+            data: '01 March 2020',
+            title: 'Fate/Stay Night: Untimated Blade World',
+            img: '/assets/blog/blog-9.jpg'
+        }, 
+        {   
+            data: '01 March 2020',
+            title: 'Yuri Kuma Arashi Viverra Tortor Pharetra',
+            img: '/assets/blog/blog-12.jpg'
+        }]
+}
 export default dataBlog;
 
 
