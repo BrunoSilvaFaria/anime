360b413c6b4b920bf05783590fdeb40bd174e9a0
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-14 12:39:09 -0300

    feat: remoção de estilos, sendo separados para arquivos externos

--- src/components/Components/Componente/HeaderSmall.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { HeaderSubtitle} from "../Styles/styles";
-export default function HeaderSmall(props) {
-    return (
-        <header>
-            <HeaderSubtitle>{props.title}</HeaderSubtitle>
-        </header>
-    )
-}
