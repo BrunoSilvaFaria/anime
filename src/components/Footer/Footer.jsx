b63347a6438fdd8100ecf13aa27e89c97aa542e6
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-19 11:00:37 -0300

    feat: melhorias no design e impplementação de button para scroll to top

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import { FooterConteiner } from "./styles";
+import { IconArrowUp } from '../../Icons'
 import logo from "../logo.png";
 export default function Header() {
+  const goToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+};
   return (
     <FooterConteiner>
-        <footer>
-          <div className="footer">
-            <img src={logo} alt="Logo do site" />
-            <nav>
-              <ul className="list">
-                <li className="link-active">
-                  <a href="#">Homepage</a>
-                </li>
-                <li>
-                  <a href="#">Categories </a>
-                </li>
-                <li>
-                  <a href="#">Our bloc</a>
-                </li>
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-              </ul>
-            </nav>
-            <p>
-              Copyright ©2023 All rights reserved | This template is made with by <a>Colorlib</a>
-            </p>
-          </div>
-        </footer>
+      <button onClick={goToTop}>
+        <IconArrowUp className="icon"/>
+      </button>
+          <section className="conteiner">
+        <div className="footer">
+              <Link to="/">
+                <img src={logo} alt="Logo do site" />
+              </Link>
+              <nav>
+                <ul className="list">
+                  <li className="link-active">
+                    <Link to="/">
+                    Homepage
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/animeCategorie">
+                    Categories 
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">
+                    Our bloc
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                    Contact
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <p>
+                Copyright ©2023 All rights reserved | This template is made with by <a>Colorlib</a>
+              </p>
+            </div>
+          </section>
       </FooterConteiner>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,30 +1,72 @@
-import styled from "styled-components";
-export const FooterConteiner= styled.header`
+import { styled, css } from "styled-components";
+export const display = css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+export const FooterConteiner= styled.footer`
     height: 190px;
     background: #070720;
     font-size: 15px;
     font-weight: 700;
     margin-top: 50px;
-
-    footer{
-        width: 90%;
+    button{
+        background: #e53637;
+        border-radius: 50%;
+        margin: auto;
+        display: block;
+        position: relative;
+        bottom: 25px;
+        width: 50px;
+        height: 50px;
+    }
+    .icon{
+        font-size: 32px;
+    }
+    .conteiner{
+        width: 95%;
         max-width: 1140px;
         margin: auto;
-        margin-top: 50px;
     }
-    .footer,
-    .list{
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
+     @media (max-width: 989px) {
+         &{
+             height: inherit;
+         }
+         button{
+             margin: 0 auto 15px auto;
+            }
+            img, .list, p{
+                margin: auto;
+        }
+        .list{
+            margin: 25px auto;
+        }
+        
+        img{
+            display: block;
+        }
+     }
+    @media (min-width: 990px) {
+        .conteiner{
+            ${display};
+            height: 100px;
+        }
+        .footer{
+            ${display};
+            align-items: flex-start;
+            height: 75px;
+            width: 100%;
+        }
+        p{
+            width: 260px;
+        }   
     }
 
-    .footer{
-        margin: auto;
-        align-items: self-start;
-    }
     .list{
-        width: 600px;
+        ${display};
+        flex-wrap: wrap;
+        width: 90%;
+        max-width: 480px;
         li{
             a{
                 color: #D9D9D9;
@@ -36,7 +78,8 @@ export const FooterConteiner= styled.header`
     }
 
     p{
-        width: 260px;
+        width: 90%;
+        max-width: 690px;
         font-size: 15px;
         color: #3d3d3d;
         line-height: 25px;
